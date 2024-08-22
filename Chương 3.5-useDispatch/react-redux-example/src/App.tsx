30f8143de45fca6b8c5dccc9ecdf4b809523d55d
TranDatk <hgooshvhd123@gmail.com>
2024-08-22 21:18:33 +0700

    Create README.md - useDispatch 👙

---
+++ Chương 3.5-useDispatch/react-redux-example/src/App.tsx
@@ -0,0 +1,35 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from './redux/store'
+import { increment } from './redux/counter/counter.slice'
+
+function App() {
+  const count = useSelector((state: RootState) => state.counter)
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>
+        My current count = {count.value}
+      </h1>
+      <button onClick={
+        () => {
+          dispatch(increment())
+        }
+      }>
+        Increase + 1
+      </button>
+    </>
+  )
+}
+
+export default App
