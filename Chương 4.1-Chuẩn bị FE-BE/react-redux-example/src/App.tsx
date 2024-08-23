3094284489371d4d2e87cbc1b20e0ff6d30db1b8
TranDatk <hgooshvhd123@gmail.com>
2024-08-23 10:53:23 +0700

    Create READNE.md - Prepare FE-BE base 🎠

---
+++ Chương 4.1-Chuẩn bị FE-BE/react-redux-example/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/header"
+import TabsContent from "./components/tabs.content"
+import UsersTable from "./components/users.table"
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TabsContent />
+      <UsersTable />
+    </>
+  )
+}
+
+export default App
